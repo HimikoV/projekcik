b0c3a1a0e1b089c82b66a7cd5b4d842f732a2649
HimikoV <48067467+HimikoV@users.noreply.github.com>
2020-01-08 21:42:56 +0100

    Update gasior.py

--- gasior.py
+++ gasior.py
@@ -1,9 +1,8 @@
 import numpy as np
 import random
 
-
-# średnia ilość klientów na minute = .15
-klientow=0.05
+"""
+# średnia ilość klientów na minute = .15, założenie że 1 na 3 klientów jest biznesowy 
 # sredni czas obslugi 10 - normalny, losujemy między 5 a 15
 # sredni czas obslugi 30 - biznesowy losujemy miedzy 20 a 40
 # sredni zarobek za klienta normalnego - 65 (losujemy pomiędzy 40 a 90)
@@ -14,24 +13,26 @@ klientow=0.05
 # każda dodatkowa osoba w kolejce zwiększa ryzyko odejscia następnego klienta
 # jak dojdzie do 10 straconych klientów, koniec symulacji, wynikiem jest uzyskany profit na godzine
 # dodatkowy warunek stopu to osiągniecie 300% zysku
-
-
+"""
+#klasa stworzona do tworzenia obiektu klienta
 class Client():
     def __init__(self, category):
         self.category = category
-
+    #zarobki pracowników
     normal_employee_cost = 2000 / 160
     qualified_employee_cost = 5000 / 160
-
+    #zwraca True jeśli klient biznesowe, False dla normalnego
     def queue_check(self):
         if self.category > .66:
             return 1
         return 0
 
+#do wyszukiwania indexów
 def deep_index(lst, w):
     return [(i, sub.index(w)) for (i, sub) in enumerate(lst) if w in sub]
 
-
+#funckja skraca czas obslugiwannych aktualnie klientów oraz
+#usuwa już obsłużonych a za nich nalicza odpowiedni profit
 def skracanie_czasu(list, actual_profit):
     lista = []
     for i in range(len(list)):
@@ -46,21 +47,23 @@ def skracanie_czasu(list, actual_profit):
         del list[i - j]
         j += 1
     try:
-        list[deep_index(list,'kolejkaA')[0][0]][1] -= 1
+        list[deep_index(list, 'kolejkaA')[0][0]][1] -= 1
     except:
         pass
     try:
-        list[deep_index(list,'kolejkaB')[0][0]][1] -=1
+        list[deep_index(list, 'kolejkaB')[0][0]][1] -= 1
     except:
         pass
     return list, actual_profit
 
+#zwraca czy osoba się rozmyśliła po zobaczeniu zbyt długiej kolejki
 def kolejka(kolejka):
-   los = random.randint(1,100)
-   if los > 50+5*(kolejka-10):
-       return True
-   return False
+    los = random.randint(1, 100)
+    if los > 50 + 5 * (kolejka - 10):
+        return True
+    return False
 
+#zlicza aktualny stan kolejki biznesowej i kolejki normalnej
 def counter(list):
     listA = 0
     listB = 0
@@ -71,51 +74,62 @@ def counter(list):
             listB += 1
     return listA, listB
 
+#symulacja całego obiektu
 def simulation(klientow):
     list = []
     queueA = 0
     queueB = 0
     time = 0
     profit = 0
-    utracony=0
-    while time < 720:
+    utracony = 0
+    klient=0
+    while time < 720: #czas symulacji - 12h pracy
         minute_check = random.random()
         list, profit = skracanie_czasu(list, profit)
-        if minute_check > 1-klientow:
+        if minute_check > 1 - klientow: #co minute czasu symulacji czy zjawił się nowy klient w zależności od średniej klientów na minute
             los = random.random()
-            if queueB-queueA>6 and los<.66 and queueA<8:
-                list.append([Client(los), random.randint(5,15),'normalny','kolejkaA'])
-            elif queueA>=10 and los>.66:
+            klient+=1
+            if queueB - queueA > 6 and los < .66 and queueA < 8:
+                list.append([Client(los), random.randint(5, 15), 'normalny', 'kolejkaA'])
+            elif queueA >= 10 and los > .66:
                 if kolejka(queueA):
-                    list.append([Client(los), random.randint(15, 35),'biznes','kolejkaA'])
+                    list.append([Client(los), random.randint(15, 35), 'biznes', 'kolejkaA'])
                 else:
-                    utracony+=1
-            elif queueB>=10 and los <=.66:
+                    utracony += 1
+            elif queueB >= 10 and los <= .66:
                 if kolejka(queueB):
-                    list.append([Client(los), random.randint(5, 15),'normalny','kolejkaB'])
+                    list.append([Client(los), random.randint(5, 15), 'normalny', 'kolejkaB'])
                 else:
-                    utracony+=1
+                    utracony += 1
             else:
                 if los > .66:
-                    list.append([Client(los), random.randint(15, 35),'biznes','kolejkaA'])
+                    list.append([Client(los), random.randint(15, 35), 'biznes', 'kolejkaA'])
                 else:
-                    list.append([Client(los), random.randint(5, 15),'normalny','kolejkaB'])
+                    list.append([Client(los), random.randint(5, 15), 'normalny', 'kolejkaB'])
             queueA, queueB = counter(list)
         if time % 60 == 0:
             profit -= (Client.normal_employee_cost + Client.qualified_employee_cost)
         time += 1
 
-    # print(f"profit: {profit}, kolejka biznesowa: {queueA} kolejka normalna: {queueB} klientów utraconych przez rozmyslenie się: {utracony}"  )
-    return {'profit':profit,'kolejka1':queueA,'kolejka2':queueB,'utraceni':utracony}
+    # print(f"profit: {profit}, kolejka biznesowa: {queueA} kolejka normalna: {queueB}
+    # klientów utraconych przez rozmyslenie się: {utracony}"  )
+    return {'profit': profit, 'kolejka1': queueA, 'kolejka2': queueB, 'utraceni': utracony, 'iloscKlientow': klient}
 
-dane={}
-dane2={}
+
+dane = {}
+dane2 = {}
 import time
-poczatek= time.time()
-for j in range(50):
-    for i in range(1,1001):
-        dane[f"dzien{i}"]=simulation(klientow)
-    dane2[f"scenariusz{j}"]=dane
-    klientow+=0.01
-print(dane)
-print("czas: ", time.time()-poczatek)
+klientow = 0.05
+poczatek = time.time()
+for j in range(50): #ilosc wariantów (srednia czestotliwosc klientow)
+    for i in range(1, 1001):
+        dane[f"dzien{i}"] = simulation(klientow) #ilosc prob sprawdzania danej symulacji
+    dane2[f"scenariusz{j}"] = dane
+    klientow += 0.01
+print(dane2)
+print("czas: ", time.time() - poczatek) #zwraca czas pracy symulacji
+
+import pickle
+#zapisywanie zebranych danych nt profit, ilosci kolejek pod koniec dnia, utraconych klientow danego dnia oraz sredniej ilosc klientow danego dnia
+with open("dane_do_gasiora","wb") as file:
+    pickle.dump(dane2,file)
