eb5b84fe33deec31a319123dbd193c30b2526e44
HimikoV <48067467+HimikoV@users.noreply.github.com>
2020-01-25 19:32:51 +0100

    Add files via upload

--- projekt_SK_wizualizacja.py
+++ projekt_SK_wizualizacja.py
@@ -1,20 +1,29 @@
 import pickle
 import matplotlib as plt
 import numpy as np
-with open("dane_do_sk.pkl","rb") as file:
+with open("dane_do_sk2.pkl","rb") as file:
     dane = pickle.load(file)
-lista=[]
 
+with open("dane_do_sk.pkl", "rb") as file:
+    dane1 = pickle.load(file)
 
-for i in dane:
-    for j in dane[i]:
-        for k in dane[i][j]:
-            lista.append(dane[i][j][k]["profit"]*5-dane[i][j][k]["utraceni"]*100)
+def zbieranina(dane):
+    lista=[]
+    for i in dane:
+        for j in dane[i]:
+            for k in dane[i][j]:
+                lista.append(dane[i][j][k]["profit"]*5-dane[i][j][k]["utraceni"]*100)
+    return lista
 
-wskaznik=[]
-for i in range(20):
-    suma=0
-    for j in range(5000):
-        suma+=lista[5000*i+j]
-    wskaznik.append(suma/5000)
-print(wskaznik)
+def liczenie_wskaznika(lista):
+    wskaznik=[]
+    for i in range(20):
+        suma=0
+        for j in range(5000):
+            suma+=lista[5000*i+j]
+        wskaznik.append(suma/5000)
+    return wskaznik
+
+print(liczenie_wskaznika(zbieranina(dane))) #dane dla SK2
+print(liczenie_wskaznika(zbieranina(dane1))) #dane dla SK
+
